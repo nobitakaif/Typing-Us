146be2cf4b2c329d88c10cc997bdbcf6778b012c
mohd kaif <mk2818356@gmail.com>
2025-11-04 00:42:06 +0530

    added text area

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Textarea } from "@/components/ui/textarea";
 import { useEffect, useState } from "react";
 
 
@@ -9,18 +10,27 @@ export default function Home() {
   useEffect(()=>{
 
     const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws)
     ws.addEventListener("open",(event)=>{
-      ws.send("alright")
+      document.addEventListener("keydown",(e)=>{
+        ws.send(e.key)
+      })
     })
     ws.addEventListener("message",(event)=>{
       setSocket(event.data)
-      alert(event.data)
     })
-    setSocket(ws)
+    
   },[])
+  function sendKey(event:any){
+    const key = event.target.value
+    
+  }
   return (
     <>
-      {JSON.stringify(socket)}
+      <div className="h-screen w-full flex flex-col justify-center items-center gap-2">
+        <p> your content {JSON.stringify(socket)}</p>
+        <Textarea className="w-5xl text-5xl font-bold h-44" onChange={sendKey} />
+      </div>
     </>
 
   );
