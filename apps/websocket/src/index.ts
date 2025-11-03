ec09f2fc70ab18449a7a5c0bbe803685d794ab65
mohd kaif <mk2818356@gmail.com>
2025-11-04 00:42:32 +0530

    updated ws

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -4,14 +4,18 @@ const wss = new WebSocketServer({port : 8080},()=>{
     console.log("wesbsocket server is running on port 8080")
 })
 
+const content = "nobitakaif"
+
 wss.on("connection",(ws)=>{
     ws.on("error", console.error)
+    let ctr = 0
     ws.on("message",(data: string)=>{
-        if(data == "ping"){
-            ws.send("pong")
+        ws.send(content)
+        if (data == content[ctr] && ctr <= content.length){
+            ctr++;
         }
         else{
-            ws.send("ghanta")
+            console.log("congrates")
         }
         
     })
