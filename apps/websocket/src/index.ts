6ee07f00eaa32344d2a28e7cca5c5ccbdb1f7bf5
mohd kaif <mk2818356@gmail.com>
2025-11-05 23:40:34 +0530

    updated ws

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -4,18 +4,35 @@ const wss = new WebSocketServer({port : 8080},()=>{
     console.log("wesbsocket server is running on port 8080")
 })
 
-const content = "nobitakaif"
+const content = "i am good"
 
 wss.on("connection",(ws)=>{
     ws.on("error", console.error)
-    let ctr = 0
+    ws.send(JSON.stringify({
+        success : false,
+        content : content
+    }))
+    let ctr = 0;
     ws.on("message",(data: string)=>{
-        ws.send(content)
-        if (data == content[ctr] && ctr <= content.length){
+        
+        if (data == content[ctr] && ctr <content.length){
             ctr++;
+            console.log(data.toString())
+            console.log("yeah you're going right!")
         }
+        
         else{
-            console.log("congrates")
+            if(ctr >= content.length){
+                ws.send(JSON.stringify({
+                    success : true
+                }))
+                console.log("you passed")
+            }
+            else{
+                ws.send(JSON.stringify({
+                    success : false
+                }));
+            }
         }
         
     })
