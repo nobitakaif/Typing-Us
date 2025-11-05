591dec30aeb50aca497b88cb4aa14605b118427a
mohd kaif <mk2818356@gmail.com>
2025-11-05 23:39:59 +0530

    updated frontend

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,23 +1,30 @@
 "use client"
 import { Textarea } from "@/components/ui/textarea";
 import { useEffect, useState } from "react";
+import Result from "./result/page";
 
 
 export default function Home() {
 
-  const [socket,setSocket] = useState<WebSocket>()
+  const [socket,setSocket] = useState<string>()
+  const [ stats, setStats ] = useState(false)
   
   useEffect(()=>{
 
     const ws = new WebSocket("ws://localhost:8080");
-    setSocket(ws)
+    
+    
+    
+    
     ws.addEventListener("open",(event)=>{
       document.addEventListener("keydown",(e)=>{
         ws.send(e.key)
       })
     })
-    ws.addEventListener("message",(event)=>{
-      setSocket(event.data)
+    ws.addEventListener("message",async (event)=>{
+      const res = await JSON.parse(event.data)
+      setStats(res.success)
+      setSocket(res.content)
     })
     
   },[])
@@ -25,10 +32,13 @@ export default function Home() {
     const key = event.target.value
     
   }
+  if(stats){
+    return <Result/>
+  }
   return (
     <>
       <div className="h-screen w-full flex flex-col justify-center items-center gap-2">
-        <p> your content {JSON.stringify(socket)}</p>
+        <p> your content "{socket}" pls type this.</p>
         <Textarea className="w-5xl text-5xl font-bold h-44" onChange={sendKey} />
       </div>
     </>
---
+++ apps/frontend/app/result/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Result(){
+    return <div className="h-screen w-ful flex justify-center items-center">
+        <div className="p-4 border-2">
+            <h1>your final result...</h1>
+        </div>
+    </div>
+}
