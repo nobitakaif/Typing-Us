6ca301ea0c1cd1c92d85fa5910b0164ac8620ee9
mohd kaif <mk2818356@gmail.com>
2025-11-09 23:31:17 +0530

    reinitialize socket

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port : 8080},()=>{
     console.log("wesbsocket server is running on port 8080")
@@ -6,34 +6,18 @@ const wss = new WebSocketServer({port : 8080},()=>{
 
 const content = "i am good"
 
+interface Data{
+    success : boolean,
+    content? : string,
+    user : WebSocket,
+    rooms : string[]
+}
+
+const User : Data[] = []
+
 wss.on("connection",(ws)=>{
-    ws.on("error", console.error)
-    ws.send(JSON.stringify({
-        success : false,
-        content : content
-    }))
-    let ctr = 0;
-    ws.on("message",(data: string)=>{
+    ws.on("message",(data)=>{
         
-        if (data == content[ctr] && ctr <content.length){
-            ctr++;
-            console.log(data.toString())
-            console.log("yeah you're going right!")
-        }
-        
-        else{
-            if(ctr >= content.length){
-                ws.send(JSON.stringify({
-                    success : true
-                }))
-                console.log("you passed")
-            }
-            else{
-                ws.send(JSON.stringify({
-                    success : false
-                }));
-            }
-        }
         
     })
     
